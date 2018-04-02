578d89dd8608a20c05050529e0148906608d087b
damien <Damien@DESKTOP-MDKQL13>
2018-04-02 16:16:49 +0200

    Pour changer de scene dans javafx après la connexion

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,9 +1,12 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.*;
+import javafx.scene.*;
 import javafx.scene.control.*;
-import javafx.stage.Window;
+import javafx.stage.Stage;
 	 
 public class Controller {
 
@@ -21,15 +24,25 @@ public class Controller {
 		
 		@FXML
 		private void connexionAction(ActionEvent event) {
-			//Window win = but.getScene().getWindow();
 			if(id.getText().isEmpty()) {
 				error.setVisible(true);
 				password.setText("");
 			}
 			
-			if(password.getText().isEmpty() || password.getText() != "password") {
+			if(!password.getText().equals("password")) {
 				error.setVisible(true);
 				password.setText("");
+			}
+			else {
+				try {
+					Parent home_page = FXMLLoader.load(getClass().getResource("Connected.fxml"));
+					Scene home_scene = new Scene(home_page);
+					Stage home_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+					home_stage.setScene(home_scene);
+					home_stage.show();
+				} catch (IOException e) {
+					System.out.println("Impossible d'afficher la page !");
+				}
 			}
 		}
 		
